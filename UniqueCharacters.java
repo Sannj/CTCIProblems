a98eddd17a2923b25667b125f7193ec8be510b24
sanjana <sanjanabadam@gmail.com>
2016-08-05 14:21:18 -0700

    first commit

---
+++ UniqueCharacters.java
@@ -0,0 +1,30 @@
+package ctci;
+
+import java.util.Arrays;
+
+/**
+ * Created by sanjanabadam on 7/11/16.
+ */
+public class UniqueCharacters {
+    public static void main(String args[]){
+        String target = "abcdef";
+        boolean[] arr = new boolean[26];
+        Arrays.fill(arr,false);
+        int flag = 0;
+        for(int i = 0;i<target.length();i++){
+            int position = ((int)target.charAt(i)-97);
+            if(true==arr[position]){
+                flag = 1;
+            break;
+            }
+            else
+                arr[position] = true;
+
+        }
+        if(flag == 0){
+            System.out.println("Unique");
+        }
+        else
+            System.out.println("Not unique");
+    }
+}
---
+++ UniqueCharactersNoExtraDS.java
@@ -0,0 +1,26 @@
+package ctci;
+
+/**
+ * Created by sanjanabadam on 7/11/16.
+ */
+public class UniqueCharactersNoExtraDS {
+
+    public static void main(String args[]){
+        String target = "abcdefga";
+        int flag = 0;
+        for(int i = 0;i<target.length();i++){
+            for(int j = i+1;j<target.length();j++){
+                if(target.charAt(i) == target.charAt(j)){
+                    flag = 1;
+                    break;
+                }
+            }
+        }
+        if(flag == 0){
+            System.out.println("Unique");
+        }
+        else
+            System.out.println("Not unique");
+    }
+
+}
